b08bcf9130b933dfb3412e9962caa58b9e8497f9
Martin Sirringhaus <msirringhaus@suse.de>
2023-08-08 16:18:24 +0300

    Add KDE integration to Firefox
    Bug: https://bugzilla.mozilla.org/show_bug.cgi?id=140751
    Bug: https://bugzilla.suse.com/show_bug.cgi?id=170055
    
    How to apply this patch:
    
    1. Import and apply it
    2. cp browser/base/content/browser.xul browser/base/content/browser-kde.xul
    3. Find editBookmarkPanelDoneButton
    4. Replace #ifndef with #ifdef in the line above (this hanges the button order from Gnome-style to KDE-style)
    5. hg qrefresh

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -297,6 +297,13 @@ var gMainPane = {
       }, backoffTimes[this._backoffIndex]);
     }
 
+    var env = Components.classes["@mozilla.org/process/environment;1"]
+      .getService(Components.interfaces.nsIEnvironment);
+    var kde_session = 0;
+    if (env.get('KDE_FULL_SESSION') == "true") {
+      kde_session = 1;
+    }
+
     this.initBrowserContainers();
     this.buildContentProcessCountMenuList();
 
@@ -1750,6 +1757,17 @@ var gMainPane = {
       }
       try {
         shellSvc.setDefaultBrowser(false);
+        if (kde_session == 1) {
+          var shellObj = Components.classes["@mozilla.org/file/local;1"]
+            .createInstance(Components.interfaces.nsILocalFile);
+          shellObj.initWithPath("/usr/bin/kwriteconfig");
+          var process = Components.classes["@mozilla.org/process/util;1"]
+            .createInstance(Components.interfaces.nsIProcess);
+          process.init(shellObj);
+          var args = ["--file", "kdeglobals", "--group", "General", "--key",
+              "BrowserApplication", "firefox"];
+          process.run(false, args, args.length);
+        }
       } catch (ex) {
         console.error(ex);
         return;
