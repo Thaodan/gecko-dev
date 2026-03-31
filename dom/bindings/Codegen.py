353f0855b3ef171d6dc25f85403e9185cd717840
Olli Pettay <Olli.Pettay@helsinki.fi>
2026-03-31 16:13:33 +0000

    Bug 2024442, don't leak nursery wrappers on failure, r=mccr8, a=dsmith
    Differential Revision: https://phabricator.services.mozilla.com/D290311

--- dom/bindings/Codegen.py
+++ dom/bindings/Codegen.py
@@ -4683,7 +4683,7 @@ class CGWrapWithCacheMethod(CGAbstractMethod):
         failureCode = dedent(
             """
             aCache->ReleaseWrapper(aObject);
-            aCache->ClearWrapper();
+            aCache->ClearWrapperOnWrapFailure();
             return false;
             """
         )
