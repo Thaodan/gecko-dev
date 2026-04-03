904b00d6eeb3226475b911843e2ea15c8a47cb95
Daniel Holbert <dholbert@cs.stanford.edu>
2026-04-03 02:19:39 +0000

    Bug 2024425: Capture PresShell in a local variable when getting primary frame.  a=diannaS DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D290614
    
    Differential Revision: https://phabricator.services.mozilla.com/D291107

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -279,8 +279,9 @@ nsIFrame* nsIContent::GetPrimaryFrame(mozilla::FlushType aType) {
     return nullptr;
   }
 
+  RefPtr<mozilla::PresShell> presShell = frame->PresShell();
   if (aType == mozilla::FlushType::Layout) {
-    frame->PresShell()->EnsureReflowIfFrameHasHiddenContent(frame);
+    presShell->EnsureReflowIfFrameHasHiddenContent(frame);
     frame = GetPrimaryFrame();
   }
 
