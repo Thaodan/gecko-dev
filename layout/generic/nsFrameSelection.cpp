bd755d7c43231740d934f1c9cd39a6b461ad644b
Olli Pettay <Olli.Pettay@helsinki.fi>
2026-04-07 20:53:57 +0000

    Bug 2029708 - avoid unexpected selection handling with table cells, r=masayuki, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D292229

--- layout/generic/nsFrameSelection.cpp
+++ layout/generic/nsFrameSelection.cpp
@@ -1487,7 +1487,7 @@ nsresult nsFrameSelection::TakeFocus(nsIContent& aNewFocus,
     }
     case FocusMode::kExtendSelection: {
       // Now update the range list:
-      nsINode* inclusiveTableCellAncestor =
+      nsCOMPtr<nsINode> inclusiveTableCellAncestor =
           GetClosestInclusiveTableCellAncestor(&aNewFocus);
       if (mTableSelection.mClosestInclusiveTableCellAncestor &&
           inclusiveTableCellAncestor &&
