1fede7b765223743f5c40d3db0060e5f222f0616
Paul Adenot <paul@paul.cx>
2026-04-13 16:48:27 +0000

    Bug 2029440 -  a=diannaS DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D292501
    
    Differential Revision: https://phabricator.services.mozilla.com/D293932

--- dom/html/TextTrackManager.cpp
+++ dom/html/TextTrackManager.cpp
@@ -202,6 +202,7 @@ void TextTrackManager::AddCues(TextTrack* aTextTrack) {
     for (uint32_t i = 0; i < cueList->Length(); ++i) {
       mNewCues->AddCue(*cueList->IndexedGetter(i, dummy));
     }
+    RefPtr<TextTrackManager> kungFuDeathGrip(this);
     MaybeRunTimeMarchesOn();
   }
 }
@@ -226,6 +227,7 @@ void TextTrackManager::RemoveTextTrack(TextTrack* aTextTrack,
     for (uint32_t i = 0; i < removeCueList->Length(); ++i) {
       mNewCues->RemoveCue(*((*removeCueList)[i]));
     }
+    RefPtr<TextTrackManager> kungFuDeathGrip(this);
     MaybeRunTimeMarchesOn();
   }
 }
@@ -288,6 +290,7 @@ void TextTrackManager::NotifyCueAdded(TextTrackCue& aCue) {
   if (mNewCues) {
     mNewCues->AddCue(aCue);
   }
+  RefPtr<TextTrackManager> kungFuDeathGrip(this);
   MaybeRunTimeMarchesOn();
 }
 
@@ -296,6 +299,7 @@ void TextTrackManager::NotifyCueRemoved(TextTrackCue& aCue) {
   if (mNewCues) {
     mNewCues->RemoveCue(aCue);
   }
+  RefPtr<TextTrackManager> kungFuDeathGrip(this);
   MaybeRunTimeMarchesOn();
   DispatchUpdateCueDisplay();
 }
@@ -831,6 +835,7 @@ void TextTrackManager::TimeMarchesOn() {
 void TextTrackManager::NotifyCueUpdated(TextTrackCue* aCue) {
   // TODO: Add/Reorder the cue to mNewCues if we have some optimization?
   WEBVTT_LOG("NotifyCueUpdated, cue=%p", aCue);
+  RefPtr<TextTrackManager> kungFuDeathGrip(this);
   MaybeRunTimeMarchesOn();
   // For the case "Texttrack.mode = hidden/showing", if the mode
   // changing between showing and hidden, TimeMarchesOn
