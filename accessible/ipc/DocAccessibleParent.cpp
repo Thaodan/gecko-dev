df080a6bbdabc9fb530ab01c9ba4827922557d96
Eitan Isaacson <eitan@monotonous.org>
2026-04-21 13:34:18 +0000

    Bug 2029724 - Invalidate directly nested tables.  a=dmeehan
    Original Revision: https://phabricator.services.mozilla.com/D292844
    
    Differential Revision: https://phabricator.services.mozilla.com/D294731

--- accessible/ipc/DocAccessibleParent.cpp
+++ accessible/ipc/DocAccessibleParent.cpp
@@ -1088,6 +1088,8 @@ void DocAccessibleParent::Destroy() {
     RemoteAccessible* acc = iter.Get()->mProxy;
     MOZ_ASSERT(acc != this);
     if (acc->IsTable()) {
+      // Prevents the invalidation code from trying to walk up the tree.
+      acc->SetParent(nullptr);
       CachedTableAccessible::Invalidate(acc);
     }
     ProxyDestroyed(acc);
