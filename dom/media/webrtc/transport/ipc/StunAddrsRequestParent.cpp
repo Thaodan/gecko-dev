6bd903a087284935001c876fc8244c5209fa64ad
Byron Campen <docfaraday@gmail.com>
2026-04-25 13:36:10 +0000

    Bug 2032350: Remove this useless assertion.  a=dmeehan DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D295922
    
    Differential Revision: https://phabricator.services.mozilla.com/D296380

--- dom/media/webrtc/transport/ipc/StunAddrsRequestParent.cpp
+++ dom/media/webrtc/transport/ipc/StunAddrsRequestParent.cpp
@@ -42,9 +42,7 @@ StunAddrsRequestParent::StunAddrsRequestParent() : mIPCClosed(false) {
   MOZ_ASSERT(mSTSThread);
 }
 
-StunAddrsRequestParent::~StunAddrsRequestParent() {
-  ASSERT_ON_THREAD(mMainThread);
-}
+StunAddrsRequestParent::~StunAddrsRequestParent() = default;
 
 mozilla::ipc::IPCResult StunAddrsRequestParent::RecvGetStunAddrs() {
   ASSERT_ON_THREAD(mMainThread);
